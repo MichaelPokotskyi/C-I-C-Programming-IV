9adddfd5b51870c96d03e9335355d64482d51802
MiShKa <sly.booking@gmail.com>
2024-01-29 22:40:25 -0800

    pr1

--- BigInt.cpp
+++ BigInt.cpp
@@ -10,6 +10,9 @@
 #include <iostream>
 #include <iosfwd>
 #include <string>
+#include <climits>
+#include <cmath>
+#include <tuple>
 
 namespace Project1 {
     // ----------------------------------------------
@@ -31,6 +34,30 @@ namespace Project1 {
             num = "0";
         else
             num = num.substr(i);
+    }
+
+    void add_leading_zeroes(std::string& num, size_t num_zeroes) {
+        num = std::string(num_zeroes, '0') + num;
+    }
+
+    std::tuple<std::string, std::string> get_larger_and_smaller(const std::string& num1,
+                                                                const std::string& num2) {
+        std::string larger, smaller;
+        if (num1.size() > num2.size() or
+           (num1.size() == num2.size() and 
+            num1 > num2)) {
+            larger = num1;
+            smaller = num2;
+        }
+        else {
+            larger = num2;
+            smaller = num1;
+        }
+
+        // pad the smaller number with zeroes
+        add_leading_zeroes(smaller, larger.size() - smaller.size());
+
+        return std::make_tuple(larger, smaller);
     }
 
     // ----------------------------------------------
@@ -141,4 +168,108 @@ namespace Project1 {
     bool operator>=(const BigInt& num, const BigInt& num1) {
         return !(num < num1);
     }
+
+    // ----------------------------------------------
+    //              BINARY ARITHMETIC
+    // ----------------------------------------------
+
+    //const BigInt operator+(const BigInt& num, const BigInt& num1) {
+    //    // if the operands are of opposite signs, perform subtraction
+    //    if (num.sign == '+' and num1.sign == '-') {
+    //        BigInt rhs = num1;
+    //        rhs.sign = '+';
+    //        return num - rhs;
+    //    }
+    //    else if (num.sign == '-' and num1.sign == '+') {
+    //        BigInt lhs = num;
+    //        lhs.sign = '+';
+    //        return -(lhs - num1);
+    //    }
+
+    //    // identify the numbers as `larger` and `smaller`
+    //    std::string larger, smaller;
+    //    std::tie(larger, smaller) = get_larger_and_smaller(num.value, num1.value);
+
+    //    BigInt result;      // the resultant sum
+    //    result.value = "";  // the value is cleared as the digits will be appended
+    //    short carry = 0, sum;
+    //    // add the two values
+    //    for (long i = larger.size() - 1; i >= 0; i--) {
+    //        sum = larger[i] - '0' + smaller[i] - '0' + carry;
+    //        result.value = std::to_string(sum % 10) + result.value;
+    //        carry = sum / (short)10;
+    //    }
+    //    if (carry)
+    //        result.value = std::to_string(carry) + result.value;
+
+    //    // if the operands are negative, the result is negative
+    //    if (num.sign == '-' and result.value != "0")
+    //        result.sign = '-';
+
+    //    return result;
+    //}
+
+    //const BigInt operator-(const BigInt& num, const BigInt& num1) {
+    //    // if the operands are of opposite signs, perform addition
+    //    if (this->sign == '+' and num.sign == '-') {
+    //        BigInt rhs = num;
+    //        rhs.sign = '+';
+    //        return *this + rhs;
+    //    }
+    //    else if (this->sign == '-' and num.sign == '+') {
+    //        BigInt lhs = *this;
+    //        lhs.sign = '+';
+    //        return -(lhs + num);
+    //    }
+
+    //    BigInt result;      // the resultant difference
+    //    // identify the numbers as `larger` and `smaller`
+    //    std::string larger, smaller;
+    //    if (abs(*this) > abs(num)) {
+    //        larger = this->value;
+    //        smaller = num.value;
+
+    //        if (this->sign == '-')      // -larger - -smaller = -result
+    //            result.sign = '-';
+    //    }
+    //    else {
+    //        larger = num.value;
+    //        smaller = this->value;
+
+    //        if (num.sign == '+')        // smaller - larger = -result
+    //            result.sign = '-';
+    //    }
+    //    // pad the smaller number with zeroes
+    //    add_leading_zeroes(smaller, larger.size() - smaller.size());
+
+    //    result.value = "";  // the value is cleared as the digits will be appended
+    //    short difference;
+    //    long i, j;
+    //    // subtract the two values
+    //    for (i = larger.size() - 1; i >= 0; i--) {
+    //        difference = larger[i] - smaller[i];
+    //        if (difference < 0) {
+    //            for (j = i - 1; j >= 0; j--) {
+    //                if (larger[j] != '0') {
+    //                    larger[j]--;    // borrow from the j-th digit
+    //                    break;
+    //                }
+    //            }
+    //            j++;
+    //            while (j != i) {
+    //                larger[j] = '9';    // add the borrow and take away 1
+    //                j++;
+    //            }
+    //            difference += 10;   // add the borrow
+    //        }
+    //        result.value = std::to_string(difference) + result.value;
+    //    }
+    //    strip_leading_zeroes(result.value);
+
+    //    // if the result is 0, set its sign as +
+    //    if (result.value == "0")
+    //        result.sign = '+';
+
+    //    return result;
+    //}
 };
