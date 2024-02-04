a95650fb76e7774bd8b43619328dcddcda665e58
MiShKa <sly.booking@gmail.com>
2024-02-04 13:06:20 -0800

    #5 & pr1

--- HW5.cpp
+++ HW5.cpp
@@ -1,176 +1,165 @@
-// Michael Pokotskyi U09662201
-// pokotskyi.m@gmail.com
-// C I C++ Programming IV CSE-40478
-// Raymond Mitchell IV
-// BigInt.cpp
-// Win10, Visual C++ 2022, ISO C17
-// Implementation of class auto_ptr
-
-#include <iostream>
-#include <assert.h>
-// using namespace std;
-
-template <typename X>
-class auto_ptr
-{
-private:
-    X* mptr;
-
-public:
-    // Constructors
-
-    //argument list has _a, whose default value is NULL.
-    //assigns mptr the valuse _a
-    explicit auto_ptr(X* _a = 0) throw()
-        : mptr(_a) {}
-
-    //constructing object from another object of auto_ptr type.
-    //mptr now points to the object previously owned by _a, and _a
-    //now has no responsibility of managing it.
-    auto_ptr(auto_ptr& _a) throw()
-        : mptr(_a.release()) {}
-
-    //constructing object from another object of auto_ptr type havind diffrent type,
-    //type Y must be convertible to type X.
-    //mptr now points to the object previously owned by _a, and _a
-    //now has no responsibility of managing it.
-    template <typename Y>
-    auto_ptr(auto_ptr<Y>& _a) throw()
-        : mptr(_a.release()) {}
-
-    // Destructor
-
-    //deletes whatever mptr was pointing to.
-    ~auto_ptr() throw()
-    {
-        delete mptr;
-    }
-
-    // Access
-    //returns pointer this object manages.
-    X* get() const throw()
-    {
-        return mptr;
-    }
-
-    //returns value of object managed my this object.
-    //crashes if it dosen't manage anything as in that case it
-    //will point to nullpointer and it can not be derefrenced.
-    X& operator*() const throw()
-    {
-        //checking if mptr is not null.
-        assert(mptr != 0);
-        return *mptr;
-    }
-
-    //return pointer this object points to.
-    X* operator->() const throw()
-    {
-        //checking if mptr is not null.
-        assert(mptr != 0);
-        return mptr;
-    }
-
-    // Assignment
-
-    //assigning auto_ptr value of another auto_ptr of diffrent type.
-    //This object now points to object which was managed by _a.
-    //type Y must be convertible to type X.
-    //_a now has no responsibility of managing it.
-    //The object managed by this object has now been deleted, if any.
-    auto_ptr& operator=(auto_ptr& _a) throw()
-    {
-        reset(_a.release());
-        return *this;
-    }
-
-    //assigning auto_ptr value of another auto_ptr of same type.
-    //This object now points to object which was managed by _a.
-    //_a now has no responsibility of managing it.
-    //The object managed by this object, if any, has now been deleted.
-    template <typename Y>
-    auto_ptr& operator=(auto_ptr<Y>& a) throw()
-    {
-        reset(a.release());
-        return *this;
-    }
-
-    // Release & Reset
-    //This object now no longer manages any pointer, and now
-    //points to null. Copy of object owned by this is returned.
-    X* release() throw()
-    {
-        X* temp = mptr;
-        mptr = 0;
-        return temp;
-    }
-
-    //deleted the object managed by this and also assigns value to
-    //mptr of function argument _a, defaults to NULL.
-    void reset(X* _a = 0) throw()
-    {
-        if (_a != mptr)
-        {
-            delete mptr;
-            mptr = _a;
-        }
-    }
-};
-
-int main()
-{
-    std::cout << "Making auto pointer of int type\n";
-    //making int pointer.
-    int* a = new int(25);
-
-    std::cout << "Before Deletion: " << *a << "\n";
-
-    //making auto_ptr to manage above created pointer a.
-    {
-        auto_ptr<int> b(a);
-    }
-    //garbage value should be printed as a has been deleted as object managing it has been deleted.
-    std::cout << "After Deletion: " << *a << "\n";
-
-    std::cout << "Making auto pointer from another auto pointer of same type.\n";
-    //assigning a new int as old one has been deleted.
-    a = new int(50);
-    //making auto ptr from a
-    auto_ptr<int> b(a);
-
-    std::cout << "Before Deletion: " << *b << "\n";
-    std::cout << "Before Deletion(address pointed by b): " << b.get() << "\n";
-
-    //creating a int auto_pointer in below block from a auto_ptr b.
-    {
-        auto_ptr<int> c(b);
-    }
-    //garbage value should be printed as a has been deleted as object managing it has been deleted.
-    std::cout << "After Deletion: " << *a << "\n";
-
-    //now, b should points to null as after we create a object from a auto_ptr,
-    //it no longer manages it and should point to null.
-    std::cout << "After Deletion(address pointed by b): " << b.get() << "\n";
-
-    std::cout << "Making auto pointer from another auto pointer of diffrent type.\n";
-    //assigning a new int as old one has been deleted.
-    a = new int(50);
-
-    //making auto ptr from a
-    auto_ptr<int> b1(a);
-
-    std::cout << "Before Deletion: " << *b1 << "\n";
-    std::cout << "Before Deletion(address pointed by b): " << b1.get() << "\n";
-
-    //creating a const int pointer in below block from a auto_ptr of type int.
-    {
-        auto_ptr<const int> c(b1);
-    }
-
-    //garbage value should be printed as a has been deleted as object managing it has been deleted.
-    std::cout << "After Deletion: " << *a << "\n";
-
-    //now, auto should points to null as after we create a object from a auto_ptr,
-    //it no longer manages it and should point to null.
-    std::cout << "After Deletion(address pointed by b): " << b1.get() << "\n";
-}
+//// Michael Pokotskyi U09662201
+//// pokotskyi.m@gmail.com
+//// C I C++ Programming IV CSE-40478
+//// Raymond Mitchell IV
+//// BigInt.cpp
+//// Win10, Visual C++ 2022, ISO C17
+//// Implementation of class auto_ptr
+//
+//#include <iostream>
+//#include <assert.h>
+//#include "UnitTest++/src/UnitTest++.h"
+//
+//template <typename X>
+//    class auto_ptr
+//    {
+//    private:
+//        X* mptr;
+//
+//    public:
+//    // ----------------------------------------------
+//    //                 CONSTRUCTORS
+//    // ----------------------------------------------
+//    explicit auto_ptr(X* ptr = 0) throw()
+//        : mptr(ptr) {}
+//
+//    auto_ptr(auto_ptr& ptr) throw()
+//        : mptr(ptr.release()) {}
+//
+//    template <typename Y>
+//    auto_ptr(auto_ptr<Y>& ptr) throw()
+//        : mptr(ptr.release()) {}
+//
+//    // ----------------------------------------------
+//    //                  DESTRUCTOR
+//    // ----------------------------------------------
+//
+//    // always warning
+//    ~auto_ptr() throw()
+//    {
+//        delete mptr;
+//    }
+//
+//    // ----------------------------------------------
+//    //                    ACCESS
+//    // ----------------------------------------------
+//
+//    X* get() const throw()
+//    {
+//        return mptr;
+//    }
+//
+//    X& operator*() const throw()
+//    {
+//        assert(mptr != 0);
+//        return *mptr;
+//    }
+//
+//    X* operator->() const throw()
+//    {
+//        assert(mptr != NULL);
+//        return mptr;
+//    }
+//
+//    // ----------------------------------------------
+//    //                 ASSIGNMENT
+//    // ----------------------------------------------
+//
+//    auto_ptr& operator=(auto_ptr& ptr) throw()
+//    {
+//        reset(ptr.release());
+//        return *this;
+//    }
+//
+//    template <typename Y>
+//    auto_ptr& operator=(auto_ptr<Y>& ptr) throw()
+//    {
+//        reset(ptr.release());
+//        return *this;
+//    }
+//
+//    // ----------------------------------------------
+//    //               RELEASE & RESET
+//    // ----------------------------------------------
+//    
+//    X* release() throw()
+//    {
+//        X* temp = mptr;
+//        mptr = NULL;
+//        return temp;
+//    }
+//
+//    void reset(X* ptr = 0) throw()
+//    {
+//        if (ptr != mptr)
+//        {
+//            delete mptr;
+//            mptr = ptr;
+//        }
+//    }
+//};
+//
+//    // ----------------------------------------------
+//    //                  UNIT TESTS
+//    // ----------------------------------------------
+//
+//    TEST(PassingToAutoPtr) {
+//        int* a = new int(10);
+//        std::cout << "TEST PassingToAutoPtr\n";
+//        std::cout << "Deref initial pointer BEFORE passing to auto_ptr: " << *a << "\n";
+//        CHECK_EQUAL(10, *a);
+//        {
+//            auto_ptr<int> b(a);
+//            std::cout << "Deref new auto_ptr: " << *b << "\n";
+//            CHECK_EQUAL(10, *b);
+//        }
+//        std::cout << "Deref initial pointer AFTER passing to auto_ptr (garbage value should be displayed): " << *a << "\n\n";
+//    }
+//
+//    TEST(MakingOneFromAnotherSameType) {
+//        std::cout << "TEST MakingOneFromAnotherSameType\n";
+//        int* a = new int(20);
+//        std::cout << "Deref initial pointer BEFORE passing to auto_ptr: " << *a << "\n";
+//        CHECK_EQUAL(20, *a);
+//        {
+//            auto_ptr<int> b(a);
+//            auto_ptr<int> c(b);
+//            std::cout << "Deref new auto_ptr: " << *c << "\n\n";
+//            CHECK_EQUAL(20, *c);
+//        }
+//
+//    }
+//
+//    TEST(PointToZeroAfterAnotherCreation) {
+//        std::cout << "TEST PointToZeroAfterAnotherCreation\n";
+//        int* a = new int(30);
+//        {
+//            auto_ptr<int> b(a);
+//            auto_ptr<int> c(b);
+//            std::cout << "Address of first new auto_ptr: " << b.get() << "\n";
+//            std::cout << "Address of second new auto_ptr: " << c.get() << "\n";
+//            std::cout << "Deref second new auto_ptr: " << *c << "\n\n";
+//            CHECK_EQUAL(30, *c);
+//        }
+//    }
+//
+//    TEST(MakingOneFromAnotherDifferType) {
+//        std::cout << "TEST MakingOneFromAnotherDifferType\n";
+//        int* a = new int(40); 
+//        {
+//            auto_ptr<int> b(a);
+//            std::cout << "Deref initial INT pointer: " << *b << "\n";
+//            CHECK_EQUAL(40, *b);
+//            auto_ptr<const int> c(b);
+//            std::cout << "Deref second CONST INT pointer: " << *c << "\n\n";
+//            CHECK_EQUAL(40, *c);
+//        }
+//    }
+//
+//    // ----------------------------------------------
+//    // Entrypoint
+//    // ----------------------------------------------
+//    int main()
+//    {
+//       return UnitTest::RunAllTests();
+//    }
